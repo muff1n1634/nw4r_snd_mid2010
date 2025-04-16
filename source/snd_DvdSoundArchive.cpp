9874ae16e735be7c7f2d955a09129bfee95e8262
muff1nOS <19197077+muff1n1634@users.noreply.github.com>
2025-04-15 19:10:47 -0700

    clean up `snd_DvdSoundArchive.cpp`

---
+++ source/snd_DvdSoundArchive.cpp
@@ -0,0 +1,6 @@
+/* Only implemented to the extent necessary to match data sections. */
+
+#include "nw4r/snd/DvdSoundArchive.h"
+
+nw4r::snd::DvdSoundArchive::DvdSoundArchive() {}
+nw4r::snd::DvdSoundArchive::DvdFileStream::DvdFileStream() {}
