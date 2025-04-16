c7b61dda3298e1aff7cf989800d02916b4b5c7f4
muff1nOS <19197077+muff1n1634@users.noreply.github.com>
2025-04-16 12:05:59 -0700

    clean up `snd_ExternalSoundPlayer.cpp`

---
+++ include/nw4r/snd/ExternalSoundPlayer.h
@@ -0,0 +1,40 @@
+#ifndef NW4R_SND_EXTERNAL_SOUND_PLAYER_H
+#define NW4R_SND_EXTERNAL_SOUND_PLAYER_H
+
+/*******************************************************************************
+ * headers
+ */
+
+#include "BasicSound.h"
+
+/*******************************************************************************
+ * classes and functions
+ */
+
+namespace nw4r { namespace snd { namespace detail
+{
+	// [R89JEL]:/bin/RVL/Debug/mainD.elf:.debug::0x27049
+	class ExternalSoundPlayer
+	{
+	// methods
+	public:
+		// methods
+		int GetPlayingSoundCount() const { return mSoundList.GetSize(); }
+		int GetPlayableSoundCount() const { return mPlayableCount; }
+
+		bool AppendSound(BasicSound *sound);
+		void RemoveSound(BasicSound *sound);
+
+		bool detail_CanPlaySound(int startPriority);
+
+	private:
+		BasicSound *GetLowestPrioritySound();
+
+	// members
+	private:
+		BasicSound::ExtSoundPlayerPlayLinkList	mSoundList;		// size 0x0c, offset 0x00
+		int										mPlayableCount;	// size 0x04, offset 0x0c
+	}; // size 0x10
+}}} // namespace nw4r::snd::detail
+
+#endif // NW4R_SND_EXTERNAL_SOUND_PLAYER_H
