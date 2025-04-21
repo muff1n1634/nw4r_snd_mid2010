27af9caec4646e85fbcd563c14e95d9f70d76be3
muff1nOS <19197077+muff1n1634@users.noreply.github.com>
2025-04-21 15:00:00 -0700

    clean up `snd_MoveValue.cpp`

---
+++ include/nw4r/snd/MoveValue.h
@@ -0,0 +1,67 @@
+#ifndef NW4R_SND_MOVE_VALUE_H
+#define NW4R_SND_MOVE_VALUE_H
+
+/*******************************************************************************
+ * classes and functions
+ */
+
+namespace nw4r { namespace snd { namespace detail
+{
+	// [R89JEL]:/bin/RVL/Debug/mainD.elf:.debug::0x27b93, 0x28fef...
+	template <typename TValue, typename TTime>
+	class MoveValue
+	{
+	// methods
+	public:
+		// cdtors
+		MoveValue() :
+			mOrigin		(TValue()),
+			mTarget		(TValue()),
+			mFrame		(TTime ()),
+			mCounter	(TTime ())
+		{
+		}
+
+		// methods
+		TValue GetValue() const
+		{
+			if (IsFinished())
+				return mTarget;
+
+			return mOrigin + mCounter * (mTarget - mOrigin) / mFrame;
+		}
+
+		void SetTarget(TValue targetValue, TTime frames)
+		{
+			mOrigin		= GetValue();
+			mTarget		= targetValue;
+			mFrame		= frames;
+			mCounter	= TTime();
+		}
+
+		bool IsFinished() const { return mCounter >= mFrame; }
+
+		void InitValue(TValue value)
+		{
+			mOrigin		= value;
+			mTarget		= value;
+			mFrame		= TTime();
+			mCounter	= TTime();
+		}
+
+		void Update()
+		{
+			if (mCounter < mFrame)
+				mCounter++;
+		}
+
+	// members
+	private:
+		TValue	mOrigin;	// size TValue, offset 0x00
+		TValue	mTarget;	// size TValue, offset TValue
+		TTime	mFrame;		// size TTime,  offset TValue * 2
+		TTime	mCounter;	// size TTine,  offset TValue * 2 + TTime
+	}; // size TValue * 2 + TTime * 2
+}}} // namespace nw4r::snd::detail
+
+#endif // NW4R_SND_MOVE_VALUE_H
