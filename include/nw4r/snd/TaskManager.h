79e03cdceaf4178a2ff281c0a315bd39f5688fe9
muff1nOS <19197077+muff1n1634@users.noreply.github.com>
2025-05-15 00:29:52 -0700

    clean up `snd_TaskManager.cpp`

---
+++ include/nw4r/snd/TaskManager.h
@@ -0,0 +1,75 @@
+#ifndef NW4R_SND_TASK_MANAGER_H
+#define NW4R_SND_TASK_MANAGER_H
+
+/*******************************************************************************
+ * headers
+ */
+
+#include "Task.h"
+
+#if 0
+#include <revolution/OS/OSThread.h> // OSThreadQueue
+#else
+#include <context_rvl.h>
+#endif
+
+/*******************************************************************************
+ * classes and functions
+ */
+
+namespace nw4r { namespace snd { namespace detail
+{
+	// [R89JEL]:/bin/RVL/Debug/mainD.elf:.debug::0x2fd1de
+	class TaskManager
+	{
+	// enums
+	public:
+		// [R89JEL]:/bin/RVL/Debug/mainD.elf:.debug::0x2fd480
+		enum TaskPriority
+		{
+			PRIORITY_LOW,
+			PRIORITY_MIDDLE,
+			PRIORITY_HIGH,
+		};
+
+	// methods
+	public:
+		// instance accessors
+		static TaskManager &GetInstance();
+
+		// methods
+		void AppendTask(Task *task, TaskPriority priority);
+
+		Task *ExecuteTask();
+		void CancelTask(Task *task);
+		void CancelAllTask();
+
+		void WaitTask();
+		void CancelWaitTask();
+
+	private:
+		// cdtors
+		TaskManager();
+
+		// methods
+		Task *GetNextTask();
+		Task *GetNextTask(TaskPriority priority, bool doRemove);
+
+		Task *PopTask();
+
+	// static members
+	public:
+		static int const PRIORITY_NUM = 3;
+
+	// members
+	private:
+		Task::LinkList	mTaskList[PRIORITY_NUM];	// size 0x24, offset 0x00
+		Task			* volatile mCurrentTask;	// size 0x04, offset 0x24
+		bool			mCancelWaitTaskFlag;		// size 0x01, offset 0x28 // TODO: volatile? (see WaitTask)
+		/* 3 bytes padding */
+		OSThreadQueue	mAppendThreadQueue;			// size 0x08, offset 0x2c
+		OSThreadQueue	mDoneThreadQueue;			// size 0x08, offset 0x34
+	}; // size 0x3c
+}}} // namespace nw4r::snd::detail
+
+#endif // NW4R_SND_TASK_MANAGER_H
