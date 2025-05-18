7c48ac0452606d92d3ae32da4b9f054ba6c0a6e9
muff1nOS <19197077+muff1n1634@users.noreply.github.com>
2025-05-18 12:16:24 -0700

    clean up `snd_VoiceManager.cpp`

---
+++ include/nw4r/snd/VoiceManager.h
@@ -0,0 +1,69 @@
+#ifndef NW4R_SND_VOICE_MANAGER_H
+#define NW4R_SND_VOICE_MANAGER_H
+
+/*******************************************************************************
+ * headers
+ */
+
+#include <types.h> // u32
+
+#include "Voice.h"
+
+/*******************************************************************************
+ * classes and functions
+ */
+
+namespace nw4r { namespace snd { namespace detail
+{
+	// [R89JEL]:/bin/RVL/Debug/mainD.elf:.debug::0x2ffb36
+	class VoiceManager
+	{
+	// methods
+	public:
+		// instance accessors
+		static VoiceManager &GetInstance();
+
+		// methods
+		void Setup(void *mem, u32 memSize);
+		void Shutdown();
+
+		Voice::LinkList const &GetVoiceList() const { return mPrioVoiceList; }
+		u32 GetRequiredMemSize(int voiceCount);
+
+		Voice *AllocVoice(int voiceChannelCount, int voiceOutCount,
+		                  int priority, Voice::Callback *callback,
+		                  void *callbackData);
+		void FreeVoice(Voice *voice);
+
+		void StopAllVoices();
+
+		void ChangeVoicePriority(Voice *voice);
+
+		void UpdateAllVoices();
+		void UpdateAllVoicesSync(byte4_t syncFlag);
+
+		void NotifyVoiceUpdate();
+
+	private:
+		// cdtors
+		VoiceManager();
+
+		// methods
+		void AppendVoiceList(Voice *voice);
+		void RemoveVoiceList(Voice *voice);
+
+		void UpdateEachVoicePriority(Voice::LinkList::Iterator const &beginItr,
+		                             Voice::LinkList::Iterator const &endItr);
+
+		int DropLowestPriorityVoice(int priority);
+
+	// members
+	private:
+		bool			mInitialized;	// size 0x01, offset 0x00
+		/* 3 bytes padding */
+		Voice::LinkList	mPrioVoiceList;	// size 0x0c, offset 0x04
+		Voice::LinkList	mFreeVoiceList;	// size 0x0c, offset 0x10
+	}; // size 0x1c
+}}} // namespace nw4r::snd::detail
+
+#endif // NW4R_SND_VOICE_MANAGER_H
